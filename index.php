3d5657e1ba6e5a3e83b0cc4d66eb972ac89aee44
Dana Clemmer <dclemmer2@mail.greenriver.edu>
2021-02-09 14:27:08 -0500

    copied food2 to food3

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+//This is my CONTROLLER
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route (home page)
+$f3->route('GET /', function () {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Define a "order" route
+$f3->route('GET /order', function () {
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+
+//Define a "order2" route
+$f3->route('POST /order2', function () {
+    //Add data from form1 to Session array
+    //var_dump($_POST);
+    if(isset($_POST['food'])) {
+        $_SESSION['food'] = $_POST['food'];
+    }
+    if(isset($_POST['meal'])) {
+        $_SESSION['meal'] = $_POST['meal'];
+    }
+
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/form2.html');
+});
+
+//Define a "summary" route
+$f3->route('POST /summary', function () {
+    //Add data from form2 to Session array
+    if(isset($_POST['conds'])) {
+        $_SESSION['conds'] = implode(", ", $_POST['conds']);
+    }
+
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+//Run fat free
+$f3->run();
