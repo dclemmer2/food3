9671e8907e05621f1b35e173e8cd33d546354e3a
Dana Clemmer <dclemmer2@mail.greenriver.edu>
2021-02-13 23:50:58 -0500

    Add condiments validation

--- index.php
+++ index.php
@@ -31,30 +31,71 @@ $f3->route('GET|POST /order', function ($f3) {
     //var_dump($_POST);
 
     //If the form has been submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if (validFood($_POST['food'])) {
-            $_SESSION['food'] = $_POST['food'];
-        } else {
-            $f3->set('errors["food"]', "Food cannot be blank");
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //Get the data from the POST array
+        $userFood = trim($_POST['food']);
+        $userMeal = $_POST['meal'];
+
+        //if the data is valid -> add to session
+        if (validFood($userFood)) {
+            $_SESSION['food'] = $userFood;
+        } //data is not valid -> set an error in F3 hive
+        else {
+            $f3->set('errors["food"]', "Food cannot be blank and must 
+            contain only characters");
         }
-        if (isset($_POST['meal'])) {
-            $_SESSION['meal'] = $_POST['meal'];
+
+        //if the data is valid -> add to session
+        if (validMeal($userMeal)) {
+            $_SESSION['meal'] = $userMeal;
+        } //data is not valid -> set an error in F3 hive
+        else {
+            $f3->set('errors["meal"]', "Select a meal");
         }
 
         //if there are no errors, redirect to /order2
-        if(empty($f3->get('errors'))) {
-            $f3->reroute('/order2');
+        if (empty($f3->get('errors'))) {
+            $f3->reroute('/order2'); //GET
         }
     }
 
     $f3->set('meals', getMeals());
+    $f3->set('userFood', isset($userFood) ? $userFood : "");
+    $f3->set('userMeal', isset($userMeal) ? $userMeal : "");
 
+    //Display a view
     $view = new Template();
     echo $view->render('views/form1.html');
 });
 
 //Define a "order2" route
 $f3->route('GET|POST /order2', function ($f3) {
+
+    //If the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //if condiments were selected
+        if (isset($_POST['conds'])) {
+            //Get condiments from post array
+            $userCondiments = $_POST['conds'];
+
+            //if the data is valid -> add to session
+            if (validCondiments($userCondiments)) {
+                $_SESSION['conds'] = implode(", ", $userCondiments);
+            }
+
+            //data is not valid -> spoofed
+            else {
+                $f3->set('errors["conds"]', "Go away");
+            }
+        }
+
+        //If there are no errors, redirect user to summary page
+        if (empty($f3->get('errors'))) {
+            $f3->reroute('/summary');
+        }
+    }
+
     $f3->set('condiments', getCondiments());
 
     //Display a view
@@ -63,15 +104,14 @@ $f3->route('GET|POST /order2', function ($f3) {
 });
 
 //Define a "summary" route
-$f3->route('GET|POST /summary', function () {
-    //Add data from form2 to Session array
-    if(isset($_POST['conds'])) {
-        $_SESSION['conds'] = implode(", ", $_POST['conds']);
-    }
+$f3->route('GET /summary', function () {
 
     //Display a view
     $view = new Template();
     echo $view->render('views/summary.html');
+
+    //CLear the SESSION array
+    session_destroy();
 });
 
 //Run fat free
--- model/validate.php
+++ model/validate.php
@@ -4,8 +4,34 @@
  *
  */
 
-/** validFood() returns true if food is not empty */
+/** validFood() returns true if food is not empty and contains only letters */
 function validFood($food) {
     //$validFoods = array("tacos", "eggs", "pizza");
-    return !empty(trim($food)); //&& inarray(strtolower($food), $validFoods);
+    //&& inarray(strtolower($food), $validFoods);
+
+    return !empty(trim($food)) && ctype_alpha($food);
+}
+
+/** validMeal() returns true if the selected meal is in the list of valid options */
+function validMeal($meal) {
+    $validMeals = getMeals();
+    return in_array($meal, $validMeals);
+}
+
+/** validCondiments() returns true if all of the condiments are in the list of valid options */
+function validCondiments($selectedConds)
+{
+    //Get valid condiments from data layer
+    $validConds = getCondiments();
+
+    //Check every selected condiment
+    foreach ($selectedConds as $selected) {
+
+        //If the selected condiment is not in the valid list, return false
+        if (!in_array($selected, $validConds)) {
+            return false;
+        }
+    }
+    //If we haven't false by now, we're good!
+    return true;
 }
