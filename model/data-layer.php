8410870e809efb74f139d64a8fd69f7b2e7b0130
Dana Clemmer <dclemmer2@mail.greenriver.edu>
2021-02-18 15:22:12 -0500

    added DataLayer class

--- model/data-layer.php
+++ model/data-layer.php
@@ -7,11 +7,17 @@
 
 class DataLayer
 {
+    /** getMeals() returns an array of meals
+     *  @return array
+     */
     function getMeals()
     {
         return array("breakfast", "lunch", "dinner");
     }
 
+    /** getCondiments() returns an array of condiments
+     *  @return array
+     */
     function getCondiments()
     {
         return array("mayonnaise", "ketchup", "mustard", "sriracha");
